257929a2581cc91907c346c16c9b44120c0b269b
Your Name <you@example.com>
2022-07-04 06:19:50 +0000

    adding test single redis script

--- scripts/test_single_redis.sh
+++ scripts/test_single_redis.sh
@@ -1,3 +1,4 @@
+set -x
 
 host=172.31.60.65
 port=6379
@@ -7,13 +8,15 @@ cli_arr=(20 40 80)
 data_size_arr=(32 64 128)
 num_request=2000
 
+log_f='single_test'
+
 for th in ${thread_arr[@]} 
 do 
    for cli in ${cli_arr[@]} 
 	do
 	  for ds in ${data_size_arr[@]}
 	  do
-	     docker run --rm redislabs/memtier_benchmark:latest -s ${host}  -p ${port} -c ${cli} -R -d ${ds} -t $th -n ${num_request}
+	     docker run --rm redislabs/memtier_benchmark:latest -s ${host}  -p ${port} -c ${cli} -R -d ${ds} -t $th -n ${num_request}  > "${log_f}_cli${cli}_th${th}_d${ds}"
 	  done
 	done
 done
